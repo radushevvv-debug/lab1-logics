725a3ae72c43a4b6f583ddbea1b722c1209edb94
Радушев Илья <radushevvv@gmail.com>
2026-09-07 19:42:10 +0300

    добавлен цикл для вывода массива и локализация кириллицы

--- task2.c
+++ task2.c
@@ -1,8 +1,12 @@
 ﻿#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <windows.h>
 
 int main(void) {
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+    
     int arr[10];
     int n = 10;
     int i;
@@ -14,6 +18,12 @@ int main(void) {
     for (i = 0; i < n; i++) {
         arr[i] = rand() % 100;
     }
-
+    
+    printf("Массив случайных чисел: ");
+    for (i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+    
     return 0;
 }
