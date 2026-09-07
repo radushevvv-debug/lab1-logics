3b07312910cca1f9e3b74d989d6a6bda3487edc8
Радушев Илья <radushevvv@gmail.com>
2026-09-07 19:04:11 +0300

    добавлена инициализация массива случайными числами

---
+++ task2.c
@@ -0,0 +1,19 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(void) {
+    int arr[10];
+    int n = 10;
+    int i;
+    
+    srand(time(NULL));
+    
+    printf("Task 2: Инициализация массива случайными числами\n");
+    
+    for (i = 0; i < n; i++) {
+        arr[i] = rand() % 100;
+    }
+
+    return 0;
+}
