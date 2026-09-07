ce616fe91edd290b733f3386ff57f0fa9a2cdae8
Радушев Илья <radushevvv@gmail.com>
2026-09-07 19:47:57 +0300

    Task 3: скелет программы, выделение памяти через malloc

---
+++ task3.c
@@ -0,0 +1,26 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+
+int main(void) {
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+    
+    int n;
+    int *arr;
+    
+    printf("Task 3: Динамический массив произвольного размера\n");
+    printf("Введите размер массива: ");
+    scanf("%d", &n);
+    
+    arr = (int*)malloc(n * sizeof(int));
+    
+    if (arr == NULL) {
+        printf("Ошибка: не удалось выделить память\n");
+        return 1;
+    }
+    
+    printf("Память выделена: %d элементов\n", n);
+
+    return 0;
+}
