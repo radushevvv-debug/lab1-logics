ec79f12fb015678db85a494c0be30bf87800722c
Радушев Илья <radushevvv@gmail.com>
2026-09-07 20:24:11 +0300

    Task 4: добавлен скелет с двумерным массивом

---
+++ task4.c
@@ -0,0 +1,27 @@
+﻿#include <stdio.h>
+#include <windows.h>
+
+int main(void) {
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+    
+    int arr[3][4] = {
+        {1, 2, 3, 4},
+        {5, 6, 7, 8},
+        {9, 10, 11, 12}
+    };
+    int rows = 3;
+    int cols = 4;
+    int i, j;
+    
+    printf("Task 4: Сумма элементов в столбцах\n");
+    printf("Двумерный массив:\n");
+    for (i = 0; i < rows; i++) {
+        for (j = 0; j < cols; j++) {
+            printf("%4d ", arr[i][j]);
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}
