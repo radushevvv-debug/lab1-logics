607d5a2f9ddddb19973545f533175253bde0d39e
Радушев Илья <radushevvv@gmail.com>
2026-09-07 19:00:31 +0300

    добавлен скелет программы с массивом

---
+++ task1.c
@@ -0,0 +1,16 @@
+﻿#include <stdio.h>
+
+int main(void) {
+    int arr[] = {34, -5, 89, 12, 67, 0, 91, 23, 8, 56};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int i;
+    
+    printf("Task 1: Разница между max и min\n");
+    printf("Исходный массив: ");
+    for (i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+    
+    return 0;
+}
