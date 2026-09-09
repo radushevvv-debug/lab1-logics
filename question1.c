4560221019f57c3377aedce0535122c26de3ae20
Радушев Илья <radushevvv@gmail.com>
2026-09-09 18:45:19 +0300

    Задание 1: изменено выделение памяти на построчное и изменен способ нахождения элементов выше главной диагонали путём задания условия в параметре внутреннего цикла

--- question1.c
+++ question1.c
@@ -24,18 +24,34 @@ int main(void) {
         return 1;
     }   
 
-    int* matrix = (int*)malloc(n * n * sizeof(int));
+    int **matrix = (int**)malloc(n * sizeof(int*));
 
     if (matrix == NULL) {
         printf("Ошибка: не удалось выделить память\n");
         return 1;
     }
     
+    for (int i = 0; i < n; i++) {
+        matrix[i] = (int*)malloc(n * sizeof(int));
+        
+        if (matrix[i] == NULL){
+            printf("Ошибка: не удалось выделить память\n");
+
+
+        for (int k = 0; k < i; k++) {
+        free(matrix[k]);
+        }
+
+        free(matrix);
+        return 1;
+    } 
+}   
+
     printf("Сгенерированная матрица:\n");
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
-            matrix[i * n + j] = rand() % 61 - 20;
-            printf("%4d ", matrix[i * n + j]);
+            matrix[i][j] = rand() % 61 - 20;
+            printf("%4d ", matrix[i][j]);
         }
         printf("\n");
     }
@@ -43,15 +59,17 @@ int main(void) {
     printf("\nЭлементы выше главной диагонали:\n");
 
     for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            if (j > i) {
-                printf("%4d ", matrix[i * n + j]);
-                sum += matrix[i * n + j];
-            }
+        for (int j = i + 1; j < n; j++) {
+                printf("%4d ", matrix[i][j]);
+                sum += matrix[i][j];
         }
     }
 
     printf("\n\nСумма элементов выше главной диагонали: %d\n", sum);
+
+    for (int i = 0; i < n; i++) {
+        free(matrix[i]);
+    }
 
     free(matrix);
 
